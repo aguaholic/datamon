4279b638c7d71a06b7bb51c2361c019acf10fb3e
JoyceOliveira <joyterra@joyceoleirasmbp.home>
2024-08-17 15:36:17 +0200

    clean up  and add table

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//  Display at least the following fields: ID, Name, Type(s), and Sprite (image).
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [],
+  template: ` <p>table works!</p> `,
+  styleUrl: './table.component.css',
+})
+export class TableComponent {}
