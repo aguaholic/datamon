a371826a17a75f55f5e4930cbe4874335711e9cd
JoyceOliveira <joyterra@joyceoleirasmbp.home>
2024-08-17 15:36:17 +0200

    clean up  and add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TableComponent } from './table/table.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, TableComponent],
+  template: `
+    <h1 class="text-3xl font-bold underline">Datamon</h1>
+    <h2 class="">Your Pokémon db</h2>
+
+    <app-table></app-table>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'datamon';
+  title = 'Datamon';
 }
