e9aa8dcfcd01b4757a3732fc73ce3bed5bc298e9
JoyceOliveira <joyterra@joyceoleirasmbp.home>
2024-08-19 15:33:56 +0200

    style table and headings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,18 @@ import { TableComponent } from './table/table.component';
   standalone: true,
   imports: [RouterOutlet, TableComponent],
   template: `
-    <h1 class="text-red-200">Datamon</h1>
-    <h2>Your Pokémon db</h2>
+    <div class="p-5">
+      <h1
+        class="mb-6 text-4xl font-extrabold tracking-widest text-gray-900 md:text-5xl underline decoration-8 decoration-blue-500"
+      >
+        Datamon
+      </h1>
+      <h2 class="mb-6 text-2xl font-bold tracking-widest">
+        Your <span class="text-yellow-500 bg-blue-500">Pokémon</span> DB
+      </h2>
 
-    <app-table></app-table>
+      <app-table></app-table>
+    </div>
   `,
 })
 export class AppComponent {
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, inject } from '@angular/core';
 import { TableModule } from 'primeng/table';
 
 import { IPokemonListItem } from '../interfaces/pokemon.interface';
-import { PokemonService } from '../pokemon.service';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-table',
@@ -11,35 +11,55 @@ import { PokemonService } from '../pokemon.service';
   template: `
     <p-table
       dataKey="id"
+      selectionMode="single"
       [value]="pokemonList"
       [rows]="10"
       [paginator]="true"
-      [tableStyle]="{ 'min-width': '75rem' }"
+      [tableStyle]="{ 'min-width': '25rem' }"
     >
       <ng-template pTemplate="header">
-        <tr>
-          <th>Id</th>
-          <th>Sprite</th>
-          <th>Name</th>
-          <th>Type(s)</th>
+        <tr class="text-lg font-bold text-left tracking-widest">
+          <th
+            class="text-center border-b-2 border-r-2 border-blue-500 bg-blue-100"
+          >
+            Id
+          </th>
+          <th
+            class="text-center border-b-2 border-r-2 border-green-500 bg-green-100"
+          >
+            Name
+          </th>
+          <th
+            class="text-center border-b-2 border-r-2 border-red-500 bg-red-100"
+          >
+            Sprite
+          </th>
+          <th
+            class="text-center border-b-2 border-r-2 border-yellow-500 bg-yellow-100"
+          >
+            Types
+          </th>
         </tr>
       </ng-template>
 
       <ng-template pTemplate="body" let-pokemon>
-        <tr [pSelectableRow]="pokemon">
-          <td>
+        <tr
+          [pSelectableRow]="pokemon"
+          class="text-sm font-normal text-gray-700 bg-gray-50 hover:bg-lime-300 capitalize"
+        >
+          <td class="border-r-2 border-yellow-500">
             {{ pokemon.id }}
           </td>
 
-          <td>
+          <td class="border-r-2 border-blue-500 tracking-widest	">
             {{ pokemon.name }}
           </td>
 
-          <td>
-            <img [src]="pokemon.image" [alt]="pokemon.name" />
+          <td class="border-r-2 border-green-500">
+            <img class="m-auto" [src]="pokemon.image" [alt]="pokemon.name" />
           </td>
 
-          <td>
+          <td class="border-r-2 border-red-500 tracking-widest">
             {{ pokemon.types }}
           </td>
         </tr>
